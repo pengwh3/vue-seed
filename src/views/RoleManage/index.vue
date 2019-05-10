f9de81141decf5213dc8e33ec513e4a54dcd7176
hanzx <3165351079@qq.com>
2019-05-10 18:31:01 +0800

    增添用户管理和角色管理的页面

---
+++ src/views/RoleManage/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        角色管理
+      <el-button type="primary">增加</el-button>
+      <el-button type="danger">删除</el-button>
+      <el-button type="warning">修改</el-button>
+    </div>
+</template>
+<script>
+import ContactCard from '@/components/ContactCard'
+export default {
+    name: 'about',
+    components: {
+        ContactCard,
+    }
+};
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/UserManage/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        用户管理
+        <contact-card></contact-card>
+    </div>
+</template>
+<script>
+import ContactCard from '@/components/ContactCard'
+export default {
+    name: 'about',
+    components: {
+        ContactCard,
+    }
+};
+</script>
+<style scoped lang="scss">
+
+</style>
